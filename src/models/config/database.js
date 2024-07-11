c677c76659d5704ca8621d21a93ac5393b549ce4
DevClaudioFilho <devclaudiofilho@gmail.com>
2024-07-11 18:34:58 +0100

    first commit

--- src/models/config/database.js
+++ src/models/config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
 'ai2_postgres',
 '5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM',
 {
-host: 'postgresql://ai2_postgres:5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM@dpg-cq818n2j1k6c738rq00g-a/ai2_postgres',
+host: 'postgresql://ai2_postgres:5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM@dpg-cq818n2j1k6c738rq00g-a.oregon-postgres.render.com/ai2_postgres',
 port: '5432',
 dialect: 'postgres'
 }
