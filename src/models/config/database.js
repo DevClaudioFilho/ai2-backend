f9c25c03e0de25b965b367eb5861181e36340db8
DevClaudioFilho <devclaudiofilho@gmail.com>
2024-07-15 15:25:29 +0100

    first commit

--- src/models/config/database.js
+++ src/models/config/database.js
@@ -1,14 +1,21 @@
 var Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-'ai2_postgres',
-'ai2_postgres',
-'5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM',
-{
-host: 'postgresql://ai2_postgres:5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM@dpg-cq818n2j1k6c738rq00g-a/ai2_postgres',
-port: '5432',
-dialect: 'postgres'
-}
-);
+// const sequelize = new Sequelize(
+// 'ai2_postgres',
+// 'postgres',
+// 'postgres',
+// {
+// host: 'localhost',
+// port: '5432',
+// dialect: 'postgres'
+// }
+// );
+
+const sequelize = new Sequelize("postgresql://ai2_postgres:5zpJfmq9OvOwqiNdtlAxyRUtc6MhCyeM@dpg-cq818n2j1k6c738rq00g-a/ai2_postgres", {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 module.exports = sequelize;
